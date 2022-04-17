66abb23c6f926cd546eab7266f81f40a94d0a243
Langat20 <kipkuruie7.lang@gmail.com>
2022-04-17 16:57:30 +0300

    import homecomponent, userscomponent and repositorycomponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { RepositoryComponent } from './repository/repository.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'users', component: UsersComponent },
+  { path: 'repository', component: RepositoryComponent },
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
