99f82ea00046372903c4389314c70f5fcc8336c8
Langat20 <kipkuruie7.lang@gmail.com>
2022-04-17 17:01:19 +0300

    import homecomponent, userscomponent and repositorycomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { GitFormComponent } from './git-form/git-form.component';
+import { UsersComponent } from './users/users.component';
+import { FormsModule } from '@angular/forms';
+import { RepositoryComponent } from './repository/repository.component';
+import {UserService} from './user.service';
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    GitFormComponent,
+    UsersComponent,
+    RepositoryComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
