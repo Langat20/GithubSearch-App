705276acf0b4c2dbace36bceed21e739d71c9737
Langat20 <kipkuruie7.lang@gmail.com>
2022-04-17 17:00:26 +0300

    add some configurations

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'GithubSearch_App'`, () => {
+  it(`should have as title 'github-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('GithubSearch_App');
+    expect(app.title).toEqual('github-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('GithubSearch_App app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('github-app app is running!');
   });
 });
